39bf7594bd5430bf9b7a153ea85b562fb056640b
Oratovskii_Oleg <oleg_oratovskiy@mail.ru>
2023-11-21 01:12:41 +0300

    removed useless files && implemented CommentController

--- src/main/java/com/example/demo/repository/PostRepository.java
+++ src/main/java/com/example/demo/repository/PostRepository.java
@@ -3,8 +3,5 @@ package com.example.demo.repository;
 import com.example.demo.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface PostRepository extends JpaRepository<Post,Long> {
-    List<Post> findAllById(long id);
 }
