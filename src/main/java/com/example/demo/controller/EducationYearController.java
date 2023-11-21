24b64e90534e83f0569c67dbf89dc790dd8b76f5
necuteboy <andreychikov32@mail.ru>
2023-11-21 23:00:05 +0500

    last updates

--- src/main/java/com/example/demo/controller/EducationYearController.java
+++ src/main/java/com/example/demo/controller/EducationYearController.java
@@ -6,14 +6,11 @@ import com.example.demo.entity.EducationYear;
 import com.example.demo.service.EducationYearService;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
-
+@CrossOrigin(origins = "*")
 @RestController
 @AllArgsConstructor
 @RequestMapping("/years")
