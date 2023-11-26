93ac29281c0f53c327b389dd752b9df2f4fb6a70
necuteboy <andreychikov32@mail.ru>
2023-11-27 03:51:08 +0500

    done with security

---
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -0,0 +1,28 @@
+package com.example.demo.controller;
+
+import com.example.demo.dto.request.JwtRequest;
+import com.example.demo.dto.request.RegistrationUserDto;
+import com.example.demo.service.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/log")
+public class AuthController {
+    private final AuthService authService;
+
+    @PostMapping("/auth")
+    public ResponseEntity<?> createAuthToken(@RequestBody JwtRequest authRequest) {
+        return authService.createAuthToken(authRequest);
+    }
+
+    @PostMapping("/registration")
+    public ResponseEntity<?> createNewUser(@RequestBody RegistrationUserDto registrationUserDto) {
+        return authService.createNewUser(registrationUserDto);
+    }
+}
