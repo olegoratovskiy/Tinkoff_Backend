9856dcf703df8639524899e082ff2b2f805c5193
Muzaffar Sadullaev <90219343+muza222@users.noreply.github.com>
2023-12-08 18:59:54 +0300

    add deleted test-class

---
+++ src/test/java/DemoApplicationTests.java
@@ -0,0 +1,14 @@
+import com.example.demo.DemoApplication;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest(classes = DemoApplication.class)
+public class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
